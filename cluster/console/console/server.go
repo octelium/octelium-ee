34830e94c63e9629cb4e80a60efb6388d2945074
George B. <george@octelium.com>
2026-05-27 14:41:55 +0000

    cluster/console - rewrite server

--- cluster/console/console/server.go
+++ cluster/console/console/server.go
@@ -9,137 +9,248 @@
 package publicserver
 
 import (
+	"bytes"
 	"context"
+	"embed"
+	"encoding/json"
 	"fmt"
+	"html/template"
+	"io/fs"
 	"net/http"
-	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
-	"github.com/octelium/octelium-ee/cluster/common/octeliumc"
-	"github.com/octelium/octelium/apis/rsc/rmetav1"
-	"github.com/octelium/octelium/cluster/common/commoninit"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/octelium/octelium/apis/main/corev1"
 	"github.com/octelium/octelium/cluster/common/healthcheck"
-	"github.com/octelium/octelium/cluster/common/httputils"
+	"github.com/octelium/octelium/cluster/common/octeliumc"
 	"github.com/octelium/octelium/cluster/common/vutils"
+	"github.com/octelium/octelium/pkg/utils/utilrand"
+	"github.com/patrickmn/go-cache"
+	"github.com/pkg/errors"
 	"go.uber.org/zap"
 )
 
-type Server struct {
+//go:embed web
+var fsWeb embed.FS
+
+type server struct {
+	domain    string
 	octeliumC octeliumc.ClientInterface
-
-	svcUID string
-
-	clusterDomain string
-	// genCache      *cache.Cache
-
-	rootURL string
+	genCache  *cache.Cache
 }
 
-func newServer(ctx context.Context, octeliumC octeliumc.ClientInterface) (*Server, error) {
+type templateGlobals struct {
+	Cluster templateGlobalsCluster `json:"cluster,omitempty"`
+}
 
-	var err error
-	ret := &Server{
+type templateGlobalsCluster struct {
+	Domain string `json:"domain,omitempty"`
+}
+
+var indexTmpl = template.Must(template.New("state").Parse(
+	`<script nonce="{{ .Nonce }}">window.__OCTELIUM_GLOBALS__ = {{ .Globals }}</script>`,
+))
+
+func initServer(ctx context.Context,
+	octeliumC octeliumc.ClientInterface,
+	clusterCfg *corev1.ClusterConfig) (*server, error) {
+
+	ret := &server{
+		domain:    clusterCfg.Status.Domain,
 		octeliumC: octeliumC,
-		svcUID:    os.Getenv("OCTELIUM_SVC_UID"),
-		// genCache:  cache.New(cache.NoExpiration, 1*time.Minute),
+		genCache:  cache.New(cache.NoExpiration, 1*time.Minute),
 	}
 
-	cc, err := octeliumC.CoreV1Utils().GetClusterConfig(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	ret.clusterDomain = cc.Status.Domain
-
-	svc, err := octeliumC.CoreC().GetService(ctx, &rmetav1.GetOptions{
-		Uid: ret.svcUID,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	ret.rootURL = fmt.Sprintf("https://%s", vutils.GetServicePublicFQDN(svc, ret.clusterDomain))
+	ret.setTemplateGlobals(clusterCfg)
 
 	return ret, nil
 }
 
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	switch {
-
-	case r.Method == "GET" && strings.HasPrefix(r.URL.Path, "/assets/"):
-		s.handleAsset(w, r)
-		return
-	case r.Method == "GET":
-		s.handleIndex(w, r)
-		return
-	default:
-		w.WriteHeader(http.StatusNotFound)
+func (s *server) setTemplateGlobals(clusterCfg *corev1.ClusterConfig) {
+	t := &templateGlobals{
+		Cluster: templateGlobalsCluster{
+			Domain: clusterCfg.Status.Domain,
+		},
 	}
+	s.genCache.Set("template-globals", t, cache.NoExpiration)
 }
 
-func (s *Server) Run(ctx context.Context) error {
+func (s *server) getTemplateGlobals() *templateGlobals {
+	val, found := s.genCache.Get("template-globals")
+	if !found {
+		return nil
+	}
+	return val.(*templateGlobals)
+}
 
-	srvErr := make(chan error)
+func (s *server) setSecurityHeaders(w http.ResponseWriter, nonce string) {
+	csp := strings.Join([]string{
+		"default-src 'none'",
+		fmt.Sprintf("script-src 'self' 'nonce-%s'", nonce),
+		"style-src 'self' 'unsafe-inline'",
+		"img-src 'self' data: https:",
+		"font-src 'self'",
+		fmt.Sprintf("connect-src 'self' https://octelium-api.%s", s.domain),
+		"frame-src 'none'",
+		"frame-ancestors 'none'",
+		"object-src 'none'",
+		"base-uri 'none'",
+		"form-action 'self'",
+		"manifest-src 'self'",
+	}, "; ")
 
-	handler, err := s.getHTTPHandler(ctx)
+	w.Header().Set("Content-Security-Policy", csp)
+	w.Header().Set("X-Frame-Options", "DENY")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+	w.Header().Set("Permissions-Policy", "camera=(), microphone=(), geolocation=(), payment=()")
+	w.Header().Set("Cache-Control", "no-store")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+}
+
+func (s *server) renderIndex(w http.ResponseWriter) {
+	nonce := utilrand.GetRandomStringCanonical(24)
+
+	blob, err := fs.ReadFile(fsWeb, filepath.Join("web", "index.html"))
 	if err != nil {
-		return err
+		zap.L().Error("Could not read index.html", zap.Error(err))
+		w.WriteHeader(http.StatusNotFound)
+		return
 	}
 
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(blob))
+	if err != nil {
+		zap.L().Error("Could not parse index.html", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	globalsJSON, err := json.Marshal(s.getTemplateGlobals())
+	if err != nil {
+		zap.L().Error("Could not marshal globals", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var scripts bytes.Buffer
+	if err := indexTmpl.Execute(&scripts, struct {
+		Nonce   string
+		Globals template.JS
+	}{
+		Nonce:   nonce,
+		Globals: template.JS(globalsJSON),
+	}); err != nil {
+		zap.L().Error("Could not execute template", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	head := doc.Find("head").First()
+	if head.Length() == 0 {
+		zap.L().Error("No <head> in index.html")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	head.AppendHtml(scripts.String())
+
+	doc.Find("script[src]").Each(func(_ int, sel *goquery.Selection) {
+		sel.SetAttr("nonce", nonce)
+	})
+	doc.Find("link[rel='modulepreload']").Each(func(_ int, sel *goquery.Selection) {
+		sel.SetAttr("nonce", nonce)
+	})
+
+	var out bytes.Buffer
+	out.WriteString("<!DOCTYPE html>")
+	if err := goquery.Render(&out, head.Parent()); err != nil {
+		zap.L().Error("Could not render index.html", zap.Error(err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	s.setDomainCookie(w)
+	s.setSecurityHeaders(w, nonce)
+	w.Write(out.Bytes())
+}
+
+func (s *server) setDomainCookie(w http.ResponseWriter) {
+	http.SetCookie(w, &http.Cookie{
+		Name:     "octelium_domain",
+		Value:    s.domain,
+		Secure:   true,
+		Domain:   s.domain,
+		Path:     "/",
+		SameSite: http.SameSiteNoneMode,
+	})
+}
+
+func (s *server) handleIndex(w http.ResponseWriter, r *http.Request) {
+	s.renderIndex(w)
+}
+
+func (s *server) handleStatic() http.Handler {
+	subFS, err := fs.Sub(fsWeb, "web")
+	if err != nil {
+		zap.L().Fatal("Could not initialize static fs", zap.Error(err))
+	}
+	return http.FileServer(http.FS(subFS))
+}
+
+func (s *server) getMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("GET /assets/{file}", s.handleStatic())
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			s.handleIndex(w, r)
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+	return mux
+}
+
+func (s *server) run(ctx context.Context) error {
 	go func() {
 		srv := &http.Server{
-			Handler:           handler,
+			Handler:           s.getMux(),
 			Addr:              vutils.ManagedServiceAddr,
 			WriteTimeout:      15 * time.Second,
-			ReadHeaderTimeout: 15 * time.Second,
+			ReadTimeout:       15 * time.Second,
+			ReadHeaderTimeout: 5 * time.Second,
+			IdleTimeout:       60 * time.Second,
+			MaxHeaderBytes:    32 * 1024,
 		}
-
-		if err := srv.ListenAndServe(); err != nil {
-			srvErr <- err
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			zap.L().Error("Console HTTP server exited", zap.Error(err))
 		}
 	}()
-
 	return nil
 }
 
-func (s *Server) getHTTPHandler(ctx context.Context) (http.Handler, error) {
-	chain := httputils.New()
-
-	handler, err := chain.Then(s)
-	if err != nil {
-		return nil, err
-	}
-
-	handler = http.AllowQuerySemicolons(handler)
-
-	return handler, nil
-}
-
 func Run(ctx context.Context) error {
-	octeliumC, err := octeliumc.NewClient(ctx, nil)
+	octeliumC, err := octeliumc.NewClient(ctx)
 	if err != nil {
 		return err
 	}
 
-	if err := commoninit.Run(ctx, nil); err != nil {
-		return err
-	}
-
-	s, err := newServer(ctx, octeliumC)
+	clusterCfg, err := octeliumC.CoreV1Utils().GetClusterConfig(ctx)
 	if err != nil {
 		return err
 	}
 
-	if err := s.Run(ctx); err != nil {
+	s, err := initServer(ctx, octeliumC, clusterCfg)
+	if err != nil {
+		return err
+	}
+
+	if err := s.run(ctx); err != nil {
 		return err
 	}
 
 	healthcheck.Run(vutils.HealthCheckPortManagedService)
-	zap.L().Info("Console is running...")
-
+	zap.L().Info("Console is running")
 	<-ctx.Done()
-
 	return nil
 }
--- cluster/console/console/server_test.go
+++ cluster/console/console/server_test.go
@@ -9,9 +9,44 @@
 package publicserver
 
 import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/octelium/octelium/cluster/common/tests"
+	"github.com/patrickmn/go-cache"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestServer(t *testing.T) {
+func TestRenderIndex(t *testing.T) {
+	ctx := context.Background()
 
+	tst, err := tests.Initialize(nil)
+	assert.Nil(t, err)
+	t.Cleanup(func() {
+		tst.Destroy()
+	})
+	fakeC := tst.C
+	clusterCfg, err := tst.C.OcteliumC.CoreV1Utils().GetClusterConfig(ctx)
+	assert.Nil(t, err)
+
+	srv, err := initServer(ctx, fakeC.OcteliumC, clusterCfg)
+	assert.Nil(t, err)
+
+	srv.genCache.Set("authserver-app-js-hash", "xxx", cache.NoExpiration)
+
+	req := httptest.NewRequest("GET", "http://localhost/", nil)
+	w := httptest.NewRecorder()
+	srv.handleIndex(w, req)
+	resp := w.Result()
+	defer resp.Body.Close()
+	_, err = io.ReadAll(resp.Body)
+	assert.Nil(t, err)
+
+	assert.True(t, strings.Contains(resp.Header.Get("Content-Security-Policy"), "frame-src"))
+
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
 }
