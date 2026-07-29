1e71903dcbb087e2682999cb5ea7462e3a4126ee
George B. <george@octelium.com>
2026-07-29 05:31:52 +0000

    cluster/cloudman - improve digitalocean provider

--- cluster/cloudman/cloudman/dnsctl/providers/digitalocean/dns.go
+++ cluster/cloudman/cloudman/dnsctl/providers/digitalocean/dns.go
@@ -23,6 +23,12 @@ import (
 	"go.uber.org/zap"
 )
 
+const (
+	recordTTL    = 300
+	listPageSize = 200
+	maxListPages = 100
+)
+
 type Provider struct {
 	c      *godo.Client
 	p      *enterprisev1.DNSProvider
@@ -38,7 +44,11 @@ func NewProvider(ctx context.Context, octeliumC octeliumc.ClientInterface, p *en
 		return nil, err
 	}
 
-	return &Provider{c: godo.NewFromToken(uenterprisev1.ToSecret(sec).GetValueStr()), domain: domain, p: p}, nil
+	return &Provider{
+		c:      godo.NewFromToken(uenterprisev1.ToSecret(sec).GetValueStr()),
+		domain: domain,
+		p:      p,
+	}, nil
 }
 
 func (p *Provider) SetCNAME(ctx context.Context, name, val string) error {
@@ -47,15 +57,9 @@ func (p *Provider) SetCNAME(ctx context.Context, name, val string) error {
 		return err
 	}
 
-	zap.S().Debugf("Got zone ID: %s", zoneID)
-
 	target := fmt.Sprintf("%s.", strings.TrimSuffix(val, "."))
 
-	if err := p.setRecords(ctx, "CNAME", zoneID, name, []string{target}); err != nil {
-		return err
-	}
-
-	return nil
+	return p.setRecords(ctx, "CNAME", zoneID, name, []string{target})
 }
 
 func (p *Provider) Set(ctx context.Context, domain string, ipAddrs []string) error {
@@ -63,8 +67,6 @@ func (p *Provider) Set(ctx context.Context, domain string, ipAddrs []string) err
 	if err != nil {
 		return err
 	}
-
-	zap.S().Debugf("Got zone ID: %s", zoneID)
 
 	v4Addrs, v6Addrs, err := splitIPAddresses(ipAddrs)
 	if err != nil {
@@ -75,26 +77,30 @@ func (p *Provider) Set(ctx context.Context, domain string, ipAddrs []string) err
 		return err
 	}
 
-	if err := p.setRecords(ctx, "AAAA", zoneID, domain, v6Addrs); err != nil {
+	return p.setRecords(ctx, "AAAA", zoneID, domain, v6Addrs)
+}
+
+func (p *Provider) Delete(ctx context.Context, domain string) error {
+	zoneID, err := p.getZoneID(ctx)
+	if err != nil {
 		return err
 	}
 
-	return nil
-}
-
-func trimDomain(arg, zoneID string) string {
-	if normalizeDomain(arg) == normalizeDomain(zoneID) {
-		return "@"
-	} else {
-		return strings.TrimSuffix(arg, fmt.Sprintf(".%s", zoneID))
+	if err := p.setRecords(ctx, "A", zoneID, domain, nil); err != nil {
+		return err
 	}
+
+	return p.setRecords(ctx, "AAAA", zoneID, domain, nil)
 }
 
 func (p *Provider) setRecords(ctx context.Context, typ, zoneID, name string, vals []string) error {
 
-	trimmedName := trimDomain(name, zoneID)
+	trimmedName, err := trimDomain(name, zoneID)
+	if err != nil {
+		return err
+	}
 
-	records, _, err := p.c.Domains.Records(ctx, zoneID, &godo.ListOptions{})
+	records, err := p.listRecords(ctx, zoneID)
 	if err != nil {
 		return err
 	}
@@ -103,7 +109,7 @@ func (p *Provider) setRecords(ctx context.Context, typ, zoneID, name string, val
 	found := make(map[string]struct{}, len(desired))
 
 	for _, record := range records {
-		if record.Name != trimmedName || record.Type != typ {
+		if record.Type != typ || !strings.EqualFold(record.Name, trimmedName) {
 			continue
 		}
 
@@ -131,7 +137,7 @@ func (p *Provider) setRecords(ctx context.Context, typ, zoneID, name string, val
 			Type: typ,
 			Name: trimmedName,
 			Data: val,
-			TTL:  300,
+			TTL:  recordTTL,
 		}); err != nil {
 			return err
 		}
@@ -142,49 +148,102 @@ func (p *Provider) setRecords(ctx context.Context, typ, zoneID, name string, val
 	return nil
 }
 
-func (p *Provider) Delete(ctx context.Context, domain string) error {
-	zoneID, err := p.getZoneID(ctx)
-	if err != nil {
-		return err
+func (p *Provider) listRecords(ctx context.Context, zoneID string) ([]godo.DomainRecord, error) {
+
+	var ret []godo.DomainRecord
+
+	opts := &godo.ListOptions{
+		Page:    1,
+		PerPage: listPageSize,
 	}
 
-	if err := p.setRecords(ctx, "A", zoneID, domain, nil); err != nil {
-		return err
+	for range maxListPages {
+		records, resp, err := p.c.Domains.Records(ctx, zoneID, opts)
+		if err != nil {
+			return nil, err
+		}
+
+		ret = append(ret, records...)
+
+		if resp == nil || resp.Links == nil || resp.Links.IsLastPage() {
+			return ret, nil
+		}
+
+		page, err := resp.Links.CurrentPage()
+		if err != nil {
+			return nil, err
+		}
+
+		opts.Page = page + 1
 	}
 
-	if err := p.setRecords(ctx, "AAAA", zoneID, domain, nil); err != nil {
-		return err
-	}
-
-	return nil
+	return nil, errors.Errorf("Too many DigitalOcean DNS record pages for the zone: %s", zoneID)
 }
 
 func (p *Provider) getZoneID(ctx context.Context) (string, error) {
 
-	zones, _, err := p.c.Domains.List(ctx, &godo.ListOptions{})
-	if err != nil {
-		return "", err
+	domain := normalizeDomain(p.domain)
+
+	var ret string
+	var retName string
+
+	opts := &godo.ListOptions{
+		Page:    1,
+		PerPage: listPageSize,
 	}
 
-	domain := normalizeDomain(p.domain)
-	var ret string
-
-	for _, zone := range zones {
-		name := normalizeDomain(zone.Name)
-		if !domainMatchesZone(domain, name) {
-			continue
+	for range maxListPages {
+		zones, resp, err := p.c.Domains.List(ctx, opts)
+		if err != nil {
+			return "", err
 		}
 
-		if len(name) > len(ret) {
-			ret = zone.Name
+		for _, zone := range zones {
+			name := normalizeDomain(zone.Name)
+			if !domainMatchesZone(domain, name) {
+				continue
+			}
+
+			if len(name) > len(retName) {
+				ret = zone.Name
+				retName = name
+			}
 		}
+
+		if resp == nil || resp.Links == nil || resp.Links.IsLastPage() {
+			break
+		}
+
+		page, err := resp.Links.CurrentPage()
+		if err != nil {
+			return "", err
+		}
+
+		opts.Page = page + 1
 	}
 
 	if ret == "" {
-		return "", errors.Errorf("Could not find domain")
+		return "", errors.Errorf("Could not find a DigitalOcean domain for: %s", p.domain)
 	}
 
+	zap.S().Debugf("Got DigitalOcean zone %s for the domain %s", ret, p.domain)
+
 	return ret, nil
+}
+
+func trimDomain(arg, zoneID string) (string, error) {
+	name := normalizeDomain(arg)
+	zone := normalizeDomain(zoneID)
+
+	if name == zone {
+		return "@", nil
+	}
+
+	if strings.HasSuffix(name, "."+zone) {
+		return name[:len(name)-len(zone)-1], nil
+	}
+
+	return "", errors.Errorf("The name %s is not within the zone %s", arg, zoneID)
 }
 
 func splitIPAddresses(ipAddrs []string) ([]string, []string, error) {
@@ -208,6 +267,9 @@ func splitIPAddresses(ipAddrs []string) ([]string, []string, error) {
 func uniqueValues(vals []string) map[string]struct{} {
 	ret := make(map[string]struct{}, len(vals))
 	for _, val := range vals {
+		if val == "" {
+			continue
+		}
 		ret[val] = struct{}{}
 	}
 	return ret
