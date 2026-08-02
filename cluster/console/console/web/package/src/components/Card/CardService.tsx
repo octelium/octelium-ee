f3c95311cf7f937b1cc32d999b6c6619f8bd82b6
George B. <george@octelium.com>
2026-08-02 13:32:36 +0000

    cluster/console - improve CardService

--- cluster/console/console/web/package/src/components/Card/CardService.tsx
+++ cluster/console/console/web/package/src/components/Card/CardService.tsx
@@ -5,96 +5,40 @@ import { getResourceRef } from "@/utils/pb";
 import { useQuery } from "@tanstack/react-query";
 import { ResourceListLabel } from "../ResourceList";
 
-const DoCardService = (props: { service: Service }) => {
-  const { service } = props;
+const DoCardService = ({ service }: { service: Service }) => {
+  const namespaceRef = service.status?.namespaceRef;
 
   return (
-    <>
-      <ResourceListLabel
-        label="Service"
-        itemRef={getResourceRef(service)}
-      ></ResourceListLabel>
-
-      <ResourceListLabel
-        label="Namespace"
-        itemRef={service.status!.namespaceRef}
-      ></ResourceListLabel>
-    </>
-  );
-
-  /*
-  return (
-    <div className="bg-gray-900 text-white py-2 px-4 shadow-lg rounded-lg inline-flex hover:shadow-2xl transition-all duration-500">
-      <div className="flex items-center justify-center">
-        {picURL.length > 0 && (
-          <img
-            src={picURL}
-            className="w-[60px] h-[60px] rounded-full shadow-md mr-2"
-          />
-        )}
-        <div>
-          <div>
-            <Link
-              to={getResourcePath(user)}
-              className="text-slate-100 hover:text-white duration-500 transition-all"
-            >
-              <div className="flex flex-col">
-                <div className="flex flex-col">
-                  {user.metadata!.displayName && (
-                    <div className="text-sm">{user.metadata!.displayName}</div>
-                  )}
-                  <div className="flex items-center">
-                    <div className="text-xs mt-1 text-slate-200">
-                      {user.metadata!.name}
-                    </div>
-                    <Badge
-                      className="mx-1"
-                      variant="outline"
-                      size="xs"
-                      color="blue"
-                    >
-                      {match(user.spec!.type)
-                        .with(CoreP.User_Spec_Type.HUMAN, () => "Human")
-                        .with(CoreP.User_Spec_Type.WORKLOAD, () => "Workload")
-                        .otherwise(() => "")}
-                    </Badge>
-                  </div>
-                </div>
-                {user.spec!.email && (
-                  <div className="text-xs text-slate-200">
-                    {user.spec!.email}
-                  </div>
-                )}
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className="ml-2"></div>
-      </div>
+    <div className="flex min-w-0 flex-wrap items-center gap-1.5 py-0.5">
+      <ResourceListLabel label="Service" itemRef={getResourceRef(service)} />
+      {(namespaceRef?.uid || namespaceRef?.name) && (
+        <ResourceListLabel label="Namespace" itemRef={namespaceRef} />
+      )}
     </div>
   );
-  */
 };
 
-const CardService = (props: { itemRef: ObjectReference }) => {
+const CardService = ({ itemRef }: { itemRef: ObjectReference }) => {
+  const refKey = itemRef.uid || itemRef.name;
   const qryService = useQuery({
-    queryKey: ["serviceCard", "service", props.itemRef.uid],
+    queryKey: ["serviceCard", "service", refKey],
     queryFn: async () => {
       const { response } = await getClientCore().getService(
-        GetOptions.create({
-          uid: props.itemRef.uid,
-        })
+        GetOptions.create({ uid: itemRef.uid, name: itemRef.name }),
       );
-
       return response;
     },
+    enabled: !!refKey,
   });
 
-  // return <></>;
+  if (qryService.data) return <DoCardService service={qryService.data} />;
 
   return (
-    qryService.data &&
-    qryService.isSuccess && <DoCardService service={qryService.data} />
+    <div className="flex min-h-6 items-center">
+      <ResourceListLabel label="Service">
+        {itemRef.name || itemRef.uid || "Unknown"}
+      </ResourceListLabel>
+    </div>
   );
 };
 
